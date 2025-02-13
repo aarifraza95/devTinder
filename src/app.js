c2d039a77c71d36ed0542c2686d517399285bc84
Mohammad Aarif <aarifraza95.ar@gmail.com>
2025-02-13 22:08:31 +0530

    setup code

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use('/test', (req,res)=>{
+    res.send('hello from test');
+})
+
+app.use('/', (req,res)=>{
+    res.send('hello from home');
+})
+
+
+app.listen(3000,() =>{
+    console.log('server is running on port 3000')
+})
