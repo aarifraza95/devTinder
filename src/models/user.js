3fcf9b9e8664d4fb927f943a4bf4b318ad1ab96a
aarifraza95 <56935276+aarifraza95@users.noreply.github.com>
2025-08-31 21:25:36 +0530

    updated models

--- src/models/user.js
+++ src/models/user.js
@@ -5,24 +5,30 @@ const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
+    required:true,
   },
   lastName: {
     type: String,
+    required:true,
   },
   gender: {
     type: String,
+    required:true,
   },
   age: {
     type: Number,
+    required:true,
   },
   about:{
     type: String,
   },
   emailId: {
     type: String,
+    required:true,
   },
   password: {
     type: String,
+    required:true,
   },
   skills:{
     type: [String],
